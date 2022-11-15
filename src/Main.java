4e25b3f05e8034fad8b15f45f4162dd04c7ed3c1
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-11-15 15:27:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int k, l = 0, m = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz: ");
+        k = input.nextInt();
+
+        for (int i = 1; i < k; i++) {
+            if (k % i == 0) {
+                l += i;
+            }
+        }
+
+        if (k == l) {
+            System.out.println(k + " Mükemmel sayıdır!");
+        } else {
+            System.out.println(k + " Mükemmel sayı değildir!");
+        }
+
     }
 }
